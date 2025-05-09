a1145f10595f19642a116970b8ac637f521fcca3
isaac <isaac@TerriTerry_2>
2025-05-09 18:42:23 -0400

    repeat hello five times

--- src/com/github/pokemonlover228/mastermind/MasterClass.java
+++ src/com/github/pokemonlover228/mastermind/MasterClass.java
@@ -4,6 +4,10 @@ public class MasterClass
 {
 	public static void main(String[] args)
 	{
+		for(int i=0; i<5; i++)
+		{
+			System.out.println("HELLO WORLD " + i);
+		}
 		System.out.println("goodbye world");
 	}
 }
